d6a49f325635a7e4fd6646b313e6b84325ac5a5f
Alonso Falconi <alonso.fl@live.com>
2021-09-05 11:46:53 -0500

    Add go back button

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,5 +1,14 @@
-const Details = () => (
-  <h1>Details</h1>
-);
+import { useHistory } from 'react-router-dom';
+
+const Details = () => {
+  const history = useHistory();
+
+  return (
+    <>
+      <h1>Details</h1>
+      <button type="button" onClick={() => history.goBack()}>Go Back</button>
+    </>
+  );
+};
 
 export default Details;
