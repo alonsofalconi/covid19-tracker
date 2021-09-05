41846abb5b2370341ab871e6f2d455c0e02f7fc2
Alonso Falconi <alonso.fl@live.com>
2021-09-05 11:37:58 -0500

    Add home and details components with routing

--- src/App.js
+++
@@ -1,5 +0,0 @@
-const App = () => (
-  <h1>COVID-19 Tracker</h1>
-);
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import Homepage from './components/Homepage';
+import Details from './components/Details';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Route exact path="/">
+        <Homepage />
+      </Route>
+      <Route path="/details">
+        <Details />
+      </Route>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
