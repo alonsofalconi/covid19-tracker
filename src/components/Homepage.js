1aceb05bc014a260ede60d30e6adcbf2528b75ab
Alonso Falconi <alonso.fl@live.com>
2021-09-05 11:43:33 -0500

    Add details link

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,10 @@
+import { Link } from 'react-router-dom';
+
 const Homepage = () => (
-  <h1>Homepage</h1>
+  <>
+    <h1>Homepage</h1>
+    <Link to="/details">Details</Link>
+  </>
 );
 
 export default Homepage;
