88cb2bc1ff23b6d36276aed835ca39c7d58d1bff
Alonso Falconi <alonso.fl@live.com>
2021-09-06 00:23:33 -0500

    Add header basic style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
-const Header = () => (
-  <header>
-    <h1>COVID-19 Tracker</h1>
-  </header>
-);
+import './Header.css';
+
+const Header = () => {
+  const backButton = 0;
+
+  return (
+    <header className="main-header">
+      <div>{backButton}</div>
+      <div>
+        <h1 className="main-header-title">COVID-19 Tracker</h1>
+      </div>
+      <div />
+    </header>
+  );
+};
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
+import Header from './components/Header';
 import Homepage from './components/Homepage';
 import Details from './components/Details';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Header />
       <Route exact path="/">
         <Homepage />
       </Route>
