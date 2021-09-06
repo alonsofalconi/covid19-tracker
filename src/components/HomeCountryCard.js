b36e11e62b28430c238863746a8c6d21e7402a7f
Alonso Falconi <alonso.fl@live.com>
2021-09-06 17:15:33 -0500

    Add right arrow icon to country cards

--- src/components/HomeCountryCard.js
+++ src/components/HomeCountryCard.js
@@ -1,16 +1,19 @@
-import PropTypes from 'prop-types';
-
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { ReactComponent as ArrowCircleRight } from './assets/icons/arrow-circle-right.svg';
 
 const HomeCountryCard = (props) => {
   const { country, newConfirmed } = props;
 
   return (
     <li key={country} className="country-card-item">
-      <Link to={{ pathname: '/details', search: `country=${country}` }}>
-        {country}
-        {newConfirmed}
-      </Link>
+      <div><ArrowCircleRight /></div>
+      <div>
+        <Link to={{ pathname: '/details', search: `country=${country}` }}>
+          {country}
+          {newConfirmed}
+        </Link>
+      </div>
     </li>
   );
 };
