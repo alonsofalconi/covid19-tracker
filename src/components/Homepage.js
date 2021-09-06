f75b42df1128da82f4876cd243de8b877c53eaf0
Alonso Falconi <alonso.fl@live.com>
2021-09-06 11:09:56 -0500

    Add dispatch to fetch data in homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,25 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { getData } from '../redux/covidData/covidData';
 
-const Homepage = () => (
-  <>
-    <h1>Homepage</h1>
-    <Link to="/details">Details</Link>
-  </>
-);
+const Homepage = () => {
+  const dispatch = useDispatch();
+
+  const data = useSelector((state) => state.covidData);
+
+  useEffect(() => {
+    if (Object.keys(data).length === 0) {
+      dispatch(getData());
+    }
+  }, []);
+
+  return (
+    <>
+      <h1>Homepage</h1>
+      <Link to="/details">Details</Link>
+    </>
+  );
+};
 
 export default Homepage;
--- src/redux/covidData/covidData.js
+++ src/redux/covidData/covidData.js
@@ -2,7 +2,7 @@ import narrativaAPI from '../../api/narrativa';
 
 const GET_DATA = 'covidTracker/covidData/GET_DATA';
 
-const initialState = [];
+const initialState = {};
 
 const dataReducer = (state = initialState, action) => {
   switch (action.type) {
