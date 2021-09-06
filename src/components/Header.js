64f82a7db0302f9b67f7639c2d3550fc688f2d9f
Alonso Falconi <alonso.fl@live.com>
2021-09-06 01:21:42 -0500

    Add go back function to header icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,16 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 import './Header.css';
 import { ReactComponent as ChevronLeftIcon } from './assets/icons/chevron-left.svg';
 
 const Header = () => {
   const location = useLocation();
+  const history = useHistory();
 
   return (
     <header className="main-header">
-      <div>
-        {location.pathname === '/details'
-          ? <ChevronLeftIcon className="chevron-left-icon" />
-          : null}
-      </div>
+      {location.pathname === '/details'
+        ? <button type="button" aria-label="back" className="main-header-back-icon" onClick={() => history.goBack()}><ChevronLeftIcon className="chevron-left-icon" /></button>
+        : <div />}
       <div>
         <h1 className="main-header-title">COVID-19 Tracker</h1>
       </div>
