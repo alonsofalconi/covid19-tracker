c96b3a1420c03268d6e9cfdd7b5864e646e46afa
Alonso Falconi <alonso.fl@live.com>
2021-09-06 15:11:04 -0500

    Add home country card component

---
+++ src/components/HomeCountryCard.js
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+
+import { Link } from 'react-router-dom';
+
+const HomeCountryCard = (props) => {
+  const { country, newConfirmed } = props;
+
+  return (
+    <li key={country}>
+      <Link to={{ pathname: '/details', search: `country=${country}` }}>
+        {country}
+        {newConfirmed}
+      </Link>
+    </li>
+  );
+};
+
+HomeCountryCard.propTypes = {
+  country: PropTypes.string.isRequired,
+  newConfirmed: PropTypes.number.isRequired,
+};
+
+export default HomeCountryCard;
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,8 +1,8 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import './Homepage.css';
 import { getData } from '../redux/covidData/covidData';
+import './Homepage.css';
+import HomeCountryCard from './HomeCountryCard';
 
 const Homepage = () => {
   const dispatch = useDispatch();
@@ -19,16 +19,13 @@ const Homepage = () => {
     <main>
       <ul className="home-list">
         <li>{data.length === 0 ? null : data[Math.round(Math.random() * data.length)][0]}</li>
+        <div>{data.length === 0 ? null : <p>STATS BY COUNTRY</p>}</div>
         {data.map((d) => (
-          <li key={d[0]}>
-            <Link to={{
-              pathname: '/details',
-              search: `country=${d[0]}`,
-            }}
-            >
-              {d[0]}
-            </Link>
-          </li>
+          <HomeCountryCard
+            key={d[0]}
+            country={d[0]}
+            newConfirmed={d[1].today_new_confirmed}
+          />
         ))}
       </ul>
     </main>
