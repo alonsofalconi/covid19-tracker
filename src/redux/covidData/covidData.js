254cbade956c0ba0edebfe1b6aa82ff054a3f909
Alonso Falconi <alonso.fl@live.com>
2021-09-06 12:13:58 -0500

    Update data structure when fetching from API

--- src/redux/covidData/covidData.js
+++ src/redux/covidData/covidData.js
@@ -2,7 +2,7 @@ import narrativaAPI from '../../api/narrativa';
 
 const GET_DATA = 'covidTracker/covidData/GET_DATA';
 
-const initialState = {};
+const initialState = [];
 
 const dataReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -20,10 +20,7 @@ const getDataAction = (payload) => ({
 
 export const getData = () => (dispatch) => {
   narrativaAPI.getDataByDate().then((res) => {
-    dispatch(getDataAction({
-      date: res.total.date,
-      countries: res.dates[res.total.date].countries,
-    }));
+    dispatch(getDataAction(Object.entries(res.dates[res.total.date].countries)));
   });
 };
 
