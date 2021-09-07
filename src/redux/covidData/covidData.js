29f47ddf73ffe25d91a10d72ec925e373467cab1
Alonso Falconi <alonso.fl@live.com>
2021-09-07 11:04:06 -0500

    Filter updating data countries

--- src/redux/covidData/covidData.js
+++ src/redux/covidData/covidData.js
@@ -7,7 +7,7 @@ const initialState = [];
 const dataReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_DATA:
-      return action.payload;
+      return action.payload.filter((country) => country[1].today_new_confirmed !== 0);
     default:
       return state;
   }
