a5206e8842ae6e6df63baacde89fb9b637165b66
Alonso Falconi <alonso.fl@live.com>
2021-09-09 00:32:23 -0500

    Remove logger from configureStore

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import dataReducer from './homepage/homepage';
 import filterReducer from './details/details';
 import countryReducer from './filter/filter';
@@ -11,6 +10,6 @@ const rootReducer = combineReducers({
   countries: countryReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
